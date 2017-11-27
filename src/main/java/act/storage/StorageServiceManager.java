9c9abbb7c563c07c4787f0bf8e87b9ba21e97540
Gelin Luo <greenlaw110@gmail.com>
2017-11-28 10:38:27 +1100

    WIP

--- src/main/java/act/storage/StorageServiceManager.java
+++ src/main/java/act/storage/StorageServiceManager.java
@@ -1,5 +1,25 @@
 package act.storage;
 
+/*-
+ * #%L
+ * ACT Storage
+ * %%
+ * Copyright (C) 2015 - 2017 ActFramework
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import act.Destroyable;
 import act.app.App;
 import act.app.AppService;
@@ -19,12 +39,12 @@ import org.osgl.util.C;
 import org.osgl.util.E;
 import org.osgl.util.S;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Singleton;
 import java.lang.annotation.Annotation;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Singleton;
 
 /**
  * Manage {@link org.osgl.storage.IStorageService} instances
